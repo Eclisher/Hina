cbf0b6f224e0f69d709f58074e26c0a743e17e88
Eclisher <hei.harizo@gmail.com>
2025-05-28 21:58:33 +0300

    chore: add Tailwind to the page

--- src/Components/Authentification.tsx
+++ src/Components/Authentification.tsx
@@ -3,42 +3,76 @@ import styles from './Auth.module.css';
 
 export default function Authentification() {
   const [isSignUp, setIsSignUp] = useState(false);
-
   const toggleForm = () => setIsSignUp(!isSignUp);
 
   return (
     <div className={styles.body_container}>
       <div className={`${styles.container} ${isSignUp ? styles.active : ''}`}>
         <div className={`${styles['form-container']} ${styles['sign-in']}`}>
-          <form>
-            <h1 className={styles.h1}>Sign In</h1>
-            <input type="email" placeholder="Email" />
-            <input type="password" placeholder="Password" />
-            <button type="submit">Sign In</button>
+          <form className="flex flex-col items-center justify-center px-10 h-full bg-white">
+            <h1 className="text-[5vh] font-bold">Sign In</h1>
+            <input
+              type="email"
+              placeholder="Email"
+              className="bg-[#e4e6d6] my-2 px-4 py-2 text-sm rounded w-full outline-none"
+            />
+            <input
+              type="password"
+              placeholder="Password"
+              className="bg-[#e4e6d6] my-2 px-4 py-2 text-sm rounded w-full outline-none"
+            />
+            <button
+              type="submit"
+              className="bg-[#000ed1] text-white text-xs font-semibold uppercase tracking-wider py-2 px-12 rounded mt-2"
+            >
+              Sign In
+            </button>
           </form>
         </div>
+
         <div className={`${styles['form-container']} ${styles['sign-up']}`}>
-          <form>
-            <h1 className={styles.h1}>Create Account</h1>
-            <input type="text" placeholder="Name" />
-            <input type="email" placeholder="Email" />
-            <input type="password" placeholder="Password" />
-            <button type="submit">Sign Up</button>
+          <form className="flex flex-col items-center justify-center px-10 h-full bg-white">
+            <h1 className="text-[5vh] font-bold">Create Account</h1>
+            <input
+              type="email"
+              placeholder="Email"
+              className="bg-[#e4e6d6] my-2 px-4 py-2 text-sm rounded w-full outline-none"
+            />
+            <input
+              type="password"
+              placeholder="Password"
+              className="bg-[#e4e6d6] my-2 px-4 py-2 text-sm rounded w-full outline-none"
+            />
+            <button
+              type="submit"
+              className="bg-[#000ed1] text-white text-xs font-semibold uppercase tracking-wider py-2 px-12 rounded mt-2"
+            >
+              Sign Up
+            </button>
           </form>
         </div>
+
         <div className={styles['toggle-container']}>
           <div className={styles.toggle}>
             <div className={`${styles['toggle-panel']} ${styles['toggle-left']}`}>
-              <h1>Welcome to HINA</h1>
-              <p className={styles.p}>Here to Sign in!</p>
-              <button type="button" onClick={toggleForm}>
+              <h1 className="text-[5vh] font-bold">Welcome to HINA</h1>
+              <p className="text-white font-[Poppins]">Here to Sign in!</p>
+              <button
+                type="button"
+                onClick={toggleForm}
+                className="bg-[#000ed1] text-white text-xs font-semibold uppercase tracking-wider py-2 px-12 rounded mt-2"
+              >
                 Sign In
               </button>
             </div>
             <div className={`${styles['toggle-panel']} ${styles['toggle-right']}`}>
-              <h1>Welcome Back!</h1>
-              <p className={styles.p}>This is a website for HEI - Tech</p>
-              <button type="button" onClick={toggleForm}>
+              <h1 className="text-[5vh] font-bold">Welcome Back!</h1>
+              <p className="text-white font-[Poppins]">This is a website for HEI - Tech</p>
+              <button
+                type="button"
+                onClick={toggleForm}
+                className="bg-[#000ed1] text-white text-xs font-semibold uppercase tracking-wider py-2 px-12 rounded mt-2"
+              >
                 Sign Up
               </button>
             </div>
